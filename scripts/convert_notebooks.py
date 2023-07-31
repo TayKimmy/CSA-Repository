a7bd170694f5811522cedd54a89f20e2fe1870b5
John Mortensen <jmortensen@powayusd.com>
2023-07-31 06:28:54 -0700

    improved regeneration functionality

--- scripts/convert_notebooks.py
+++ scripts/convert_notebooks.py
@@ -4,6 +4,7 @@ from nbconvert.utils.exceptions import ConversionException
 import os
 import nbformat
 import yaml
+import sys
 
 # Specify the directory where your Jupyter Notebook files are located
 notebook_directory = "_notebooks"
@@ -11,27 +12,36 @@ notebook_directory = "_notebooks"
 # Specify the destination directory for the converted Markdown files
 destination_directory = "_posts"
 
-def extract_front_matter(notebook):
+def error_cleanup(notebook_file):
+    # Remove the destination file if it exists
+    destination_file = os.path.basename(notebook_file).replace(".ipynb", "_IPYNB_2_.md")
+    destination_path = os.path.join(destination_directory, destination_file)
+    if os.path.exists(destination_path):
+        os.remove(destination_path)
+
+def extract_front_matter(notebook_file, cell):
     front_matter = {}
     
-    source = notebook.get('source', '')
+    source = cell.get('source', '')
     if source.startswith('---'):
         # Extract front matter from source
         try:
             front_matter = yaml.safe_load(source.split('---', 2)[1])
         except yaml.YAMLError as e:
             print(f"Error parsing YAML front matter: {e}")
-    
+            error_cleanup(notebook_file)
+            sys.exit(1)
+
     return front_matter
 
 # Function to convert the notebook to Markdown with front matter
-def convert_notebook_to_markdown_with_front_matter(notebook_file, destination_directory):
+def convert_notebook_to_markdown_with_front_matter(notebook_file):
     # Load the notebook file
     with open(notebook_file, 'r', encoding='utf-8') as file:
         notebook = nbformat.read(file, as_version=nbformat.NO_CONVERT)
         
         # Extract front matter from the first cell
-        front_matter = extract_front_matter(notebook.cells[0])
+        front_matter = extract_front_matter(notebook_file, notebook.cells[0])
         
         # Remove the first cell from the notebook
         notebook.cells.pop(0)
@@ -55,14 +65,25 @@ def convert_notebook_to_markdown_with_front_matter(notebook_file, destination_di
             file.write(markdown_with_front_matter)
 
 # Function to convert the Jupyter Notebook files to Markdown
+def convert_single_notebook(notebook_file):
+    try:
+        convert_notebook_to_markdown_with_front_matter(notebook_file)
+    except ConversionException as e:
+        print(f"Conversion error for {notebook_file}: {str(e)}")
+        error_cleanup(notebook_file)
+        sys.exit(1)
+
 def convert_notebooks():
     notebook_files = glob.glob(f"{notebook_directory}/*.ipynb")
     
     for notebook_file in notebook_files:
         try:
-            # Convert each notebook file to Markdown with front matter
-            convert_notebook_to_markdown_with_front_matter(notebook_file, destination_directory)
+            convert_single_notebook(notebook_file)
         except ConversionException as e:
             print(f"Conversion error for {notebook_file}: {str(e)}")
+            error_cleanup(notebook_file)
+            sys.exit(1)
 
-convert_notebooks()
+# Call the function to perform conversions when the script is run directly
+if __name__ == "__main__":
+    convert_notebooks()
