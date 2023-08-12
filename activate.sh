d5781e80aa2a311e71554743674b134df276f53a
John Mortensen <jmortensen@powayusd.com>
2023-08-11 21:15:05 -0700

    errors are gone

--- activate.sh
+++ activate.sh
@@ -40,5 +40,4 @@ echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
 echo "=== Gem install starting, thinking... ==="
 gem install jekyll bundler
-echo "=== Ignore Error installing jekyll ==="
 
